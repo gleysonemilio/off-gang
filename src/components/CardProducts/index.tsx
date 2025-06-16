3547632d6c918415875c1d6c1bb31e7d31a7588c
gleysonemilio <116229121+gleysonemilio@users.noreply.github.com>
2025-06-16 11:17:05 -0300

    feat (component): update component app

--- src/components/CardProducts/index.tsx
+++ src/components/CardProducts/index.tsx
@@ -5,7 +5,7 @@ export default function CardProducts() {
         <div className="relative overflow-hidden flex flex-col group gap-2 cursor-pointer">
             <Image
                 src={'https://bawclothing.fbitsstatic.net/img/p/moletom-hoodie-selfie-chenille-150349/338016.jpg?w=556&h=833&v=202506051948'} alt="Product Image"
-                width={230}
+                width={280}
                 height={500}
             />
             <div className="absolute top-[330px] left-19 bg-green-500 text-white opacity-0 group-hover:opacity-100 px-3 ">
@@ -13,10 +13,10 @@ export default function CardProducts() {
             </div>
 
             <div>
-                <p className=" text-sm ">
+                <p className="text-sm ">
                     CAMISETA REGULAR QR-CODE
                 </p>
-                <span className="stext-sm font-bold text-neutral-500">
+                <span className="text-sm font-bold text-neutral-500">
                     R$ 100,00
                 </span>
             </div>
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -6,6 +6,7 @@ import { Button } from "../ui/button";
 import Image from "next/image";
 import LogoTB from '../../assets/logo-t-b.png'
 import LogoTW from '../../assets/logo-t-w.png'
+import Link from "next/link";
 
 
 
@@ -15,20 +16,26 @@ export default function Header() {
   return (
     <header className="fixed z-50 w-[90%] bg-accent h-12 flex items-center justify-between px-4 m-4 rounded-4xl shadow-md">
 
-        <div className="flex items-center"></div>
-        <div> <Image src={theme === 'light' ? LogoTB : LogoTW} alt="Logo" width={40} height={100} className="rounded-full" /> </div>
-        <div className="flex items-center">
-          <div><CircleUserRound/></div>
-          
-         <Button
-            size="icon"
-            variant="ghost"
-            className="ml-2 h-8 w-8 rounded-full hover:bg-gray-200 dark:hover:bg-gray-700"
-            onClick={() => setTheme(theme === 'light' ? 'dark' : 'light')}
-          >
-            { theme === 'light' ?  <Moon className="forced-color-adjust-auto" /> : <Sun className="forced-color-adjust-auto" /> }
-          </Button>
+      <div className="flex items-center"></div>
+      <div>
+        <Link href={'/'}>
+          <Image src={theme === 'light' ? LogoTB : LogoTW} alt="Logo" width={40} height={100} className="rounded-full" />
+        </Link>
+      </div>
+      <div className="flex items-center">
+        <div>
+          <CircleUserRound />
         </div>
+
+        <Button
+          size="icon"
+          variant="ghost"
+          className="ml-2 h-8 w-8 rounded-full hover:bg-gray-200 dark:hover:bg-gray-700"
+          onClick={() => setTheme(theme === 'light' ? 'dark' : 'light')}
+        >
+          {theme === 'light' ? <Moon className="forced-color-adjust-auto" /> : <Sun className="forced-color-adjust-auto" />}
+        </Button>
+      </div>
     </header>
   );
 }
