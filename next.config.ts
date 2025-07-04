6db419926e5232e4096dec5548403997c85f7930
gleysonemilio <116229121+gleysonemilio@users.noreply.github.com>
2025-07-04 11:58:02 -0300

    feat (): fix erro

--- next.config.ts
+++ next.config.ts
@@ -1,17 +1,17 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
   images: {
     domains: ['bawclothing.fbitsstatic.net'],
   },
-  "printWidth": 80,
-  "tabWidth": 4,
-  "trailingComma": "all",
-  "singleQuote": true,
-  "semi": true,
-  "importOrder": ["^@core/(.*)$", "^@server/(.*)$", "^@ui/(.*)$", "^[./]"],
-  "importOrderSeparation": true,
-  "importOrderSortSpecifiers": true
-};
+  printWidth: 80,
+  tabWidth: 4,
+  trailingComma: 'all',
+  singleQuote: true,
+  semi: true,
+  importOrder: ['^@core/(.*)$', '^@server/(.*)$', '^@ui/(.*)$', '^[./]'],
+  importOrderSeparation: true,
+  importOrderSortSpecifiers: true,
+}
 
-export default nextConfig;
+export default nextConfig
