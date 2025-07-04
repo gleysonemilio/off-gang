08d7ea23c5381e2952b37b9b3fc431517f2023ba
gleysonemilio <116229121+gleysonemilio@users.noreply.github.com>
2025-07-04 20:43:39 -0300

    feat (): skeleton

---
+++ src/components/Skeleton/Skeleton-edit.tsx
@@ -0,0 +1,20 @@
+import { Skeleton } from "../ui/skeleton";
+
+export default function SkeletonEdit() {
+  return (
+    <div className="w-full flex flex-col space-y-3 gap-3">
+      <Skeleton className="h-[40px] w-[100%] rounded-xl" />
+      <div className='flex flex-row gap-2'>
+        {Array.from({ length: 5 }, () =>
+          <Skeleton className="h-8 w-[100%]" />)
+        }
+      </div>
+      <Skeleton className="h-[40px] w-[100%] rounded-xl" />
+      <div className="w-full flex flex-row space-y-3 gap-3">
+        {Array.from({ length: 2 }, () =>
+          <Skeleton className="h-48 w-[100%]" />)
+        }
+      </div>
+    </div>
+  )
+}
