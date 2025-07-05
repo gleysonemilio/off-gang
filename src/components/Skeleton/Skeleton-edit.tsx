8158790b5fdc46268af7c762ba5821b94c73568b
gleysonemilio <116229121+gleysonemilio@users.noreply.github.com>
2025-07-04 23:10:19 -0300

    feat (): add key

--- src/components/Skeleton/Skeleton-edit.tsx
+++ src/components/Skeleton/Skeleton-edit.tsx
@@ -5,14 +5,14 @@ export default function SkeletonEdit() {
     <div className="w-full flex flex-col space-y-3 gap-3">
       <Skeleton className="h-[40px] w-[100%] rounded-xl" />
       <div className='flex flex-row gap-2'>
-        {Array.from({ length: 5 }, () =>
-          <Skeleton className="h-8 w-[100%]" />)
+        {Array.from({ length: 5 }, (index) =>
+          <Skeleton key='Skeleton' className="h-8 w-[100%]" />)
         }
       </div>
       <Skeleton className="h-[40px] w-[100%] rounded-xl" />
       <div className="w-full flex flex-row space-y-3 gap-3">
         {Array.from({ length: 2 }, () =>
-          <Skeleton className="h-48 w-[100%]" />)
+          <Skeleton key='Skeleton' className="h-48 w-[100%]" />)
         }
       </div>
     </div>
